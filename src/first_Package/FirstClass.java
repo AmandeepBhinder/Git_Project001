f87e249e7fb33348ac6042cb9efa20e3a48f5f3e
Aman Bhinder <amanbhinder635@gmail.com>
2020-08-09 00:31:14 -0400

    The third commit from local master

--- src/first_Package/FirstClass.java
+++ src/first_Package/FirstClass.java
@@ -5,10 +5,17 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from local repository");
 		System.out.println("Our second commit from local repository");
+
 		System.out.println("Now I am on my Aman branch");
+
+
+		System.out.println("feature branch next commit");
+
+
 		System.out.println("I want to have conflict");
 		
 		System.out.println("I want to have a second conflict");
+
 	}
 
 }
